2af0e904131a9646c763ca8465717fab3335799b
Bart Jacobs <60138978+Cloudsparkle@users.noreply.github.com>
2022-03-15 14:38:57 +0100

    Create PS-CreateScheduledTask.ps1

---
+++ PS-CreateScheduledTask.ps1
@@ -0,0 +1,35 @@
+﻿$taskname = "Test"
+$taskdescription = "Dit is een test"
+$taskExe = "c:\program Files (x86)\BV16\BV16.exe"
+$taskExeArgument = ""
+$taskUserID = "ITGLO\sa_taskflow"
+$taskUserCred = Get-Credential -Message "Please enter password" -UserName $taskUserID
+$taskminutes = 1
+
+if ($taskExeArgument -eq "")
+{
+    $taskAction = New-ScheduledTaskAction -Execute $taskExe
+}
+else
+{
+    $taskAction = New-ScheduledTaskAction -Execute $taskExe -Argument $taskExeArgument
+}
+
+$tasktrigger = New-ScheduledTaskTrigger -Once -at (get-date).AddMinutes($taskminutes) -RepetitionInterval (new-timespan -minute $taskminutes)
+$tasksettings = New-ScheduledTaskSettingsSet -MultipleInstances IgnoreNew -ExecutionTimeLimit "00:00:00"
+
+#Register-ScheduledTask -TaskName $taskName -Action $taskAction -Trigger $taskTrigger -Description $taskdescription -User $taskUserCred.UserName -Password $taskUserCred.GetNetworkCredential().Password
+
+$params = @{
+"Action"      = $taskAction
+"TaskName"    = $taskname
+"Trigger"     = $tasktrigger
+"User"        = $taskUserCred.UserName
+"Password"    = $taskUserCred.GetNetworkCredential().Password
+"RunLevel"    = "Highest"
+"Description" = $taskdescription
+"Settings"    = $tasksettings
+}
+
+Register-ScheduledTask @params
+ 
